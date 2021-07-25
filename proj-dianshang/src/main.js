8f416fd54b0fcecd711e96f06c87de37992cb7ea
lxy <964305714@qq.com>
2021-07-25 21:56:19 +0800

    07.24

--- proj-dianshang/src/main.js
+++ proj-dianshang/src/main.js
@@ -8,6 +8,11 @@ import axios from 'axios'
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
+axios.interceptors.request.use(config => {
+  // 为请求头对象，添加token验证的Authorization字段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 
 Vue.config.productionTip = false
 
