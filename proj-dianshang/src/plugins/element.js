4fb024c01c54c9fd935241d0bbc80a24707e0071
ya7788 <68853592+ya7788@users.noreply.github.com>
2021-07-31 17:46:35 +0800

    07.31

--- proj-dianshang/src/plugins/element.js
+++ proj-dianshang/src/plugins/element.js
@@ -23,15 +23,11 @@ import {
   Tooltip,
   Pagination,
   Dialog,
-<<<<<<< HEAD
   MessageBox,
   Tag,
   Tree,
   Select,
   Option
-=======
-  MessageBox
->>>>>>> 96dc15677d551c0dca5788e850ef050e4dc89607
 } from 'element-ui'
 
 Vue.use(Button)
@@ -56,6 +52,10 @@ Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
+Vue.use(Tag)
+Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 // 挂载
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
